0ca28d20bbb8c561f24b7b261e26242faa935184
Rogerio Coelho <suporte@tidea.com.br>
2021-05-23 18:00:36 -0300

    finalizado

--- src/index.js
+++ src/index.js
@@ -10,19 +10,59 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+  if (!user){
+     return response.status(404).json({error: 'User not found !'});
+  }
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+ 
+  if (user.pro || (!user.pro && user.todos.length<10)){
+    return next();
+  }
+  else{
+    return response.status(403).json({error: 'User exceeded availability'});
+  }
+ 
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  const {id} = request.params;
+
+  if (!validate(id)){
+    return response.status(400).json({error: 'Invalid uuid'});
+  }
+
+  const user = users.find(user => user.username === username);
+  if (!user){
+    return response.status(404).json({error: 'user not found'});
+  }
+
+  const todoFromUser = user.todos.find(todo => todo.id === id);
+
+  if (!todoFromUser){
+    return response.status(404).json({error: 'Invalid todo information'});
+  }
+
+  request.user = user;
+  request.todo = todoFromUser;
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params;
+  const user = users.find(user => user.id === id);
+  if (!user){
+    return response.status(404).json({error: 'User not found'});
+  }
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
